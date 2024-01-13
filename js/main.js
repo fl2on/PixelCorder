cfebc4c4b9f31eafca33ac2cb3c0bbbca67d0a7f
Nova <69091361+qzxtu@users.noreply.github.com>
2024-01-13 05:50:00 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@ async function startRecording() {
                 }
             },
             audio: {
-                echoCancellation: true
+                echoCancellation: false
             }
         });
 
